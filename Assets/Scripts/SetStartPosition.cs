5b651bf52181e30856e0e45887e5a1dc1208166f
jandy14 <seonguk14@gmail.com>
2018-03-19 22:07:17 +0900

    *큐브 시작위치 설정

---
+++ Assets/Scripts/SetStartPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartPosition : MonoBehaviour {
+
+    [SerializeField] private Transform v;
+	// Use this for initialization
+	void Start ()
+    {
+        transform.position = v.position + (v.rotation * Vector3.forward * 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Vector3 tmp = v.rotation.eulerAngles * Mathf.Deg2Rad;
+        //Vector3 result = Vector3.zero;
+        //result += new Vector3(0, Mathf.Cos(tmp.x), Mathf.Sin(tmp.x));
+        //transform.position = v.position + result;
+        //transform.position = v.position + (v.rotation * Vector3.forward);
+	}
+}
