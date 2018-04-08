f9af696f47601cee016520ae446b1b317e4977fc
SiHeungPark <omway17@gmail.com>
2018-04-08 21:37:39 +0900

    레거시 정리
    ㅇㅇ

--- Assets/Scripts/CreativeZone_UI_Call.cs
+++ Assets/Scripts/CreativeZone_UI_Call.cs
@@ -7,7 +7,6 @@ public class CreativeZone_UI_Call : MonoBehaviour {
     public  GameObject UI;
     private SteamVR_TrackedObject trackedObj;
     private bool UI_ON = false;
-    private GameObject laser = GameObject.Find("CurvedUILaserBeam");
 
     private SteamVR_Controller.Device Controller
     {
@@ -27,7 +26,6 @@ public class CreativeZone_UI_Call : MonoBehaviour {
             {
                 UI.transform.position = v.position + new Vector3(0, 2000, 0);
                 UI_ON = false;
-                laser.gameObject.SetActive(UI_ON);
             }
             else
             {
@@ -35,7 +33,6 @@ public class CreativeZone_UI_Call : MonoBehaviour {
 				UI.transform.eulerAngles = new Vector3(UI.transform.eulerAngles.x, UI.transform.eulerAngles.y,0f);
                 UI.transform.position = v.position + (v.rotation * Vector3.forward * 2.0f);
                 UI_ON = true;
-                laser.gameObject.SetActive(UI_ON);
 
             }
 
