97c1b623b48f070a5a047fee68f78876e9080c35
jandy14 <seonguk14@gmail.com>
2018-11-06 16:34:17 +0900

    *메뉴에서 제작한 오브젝트 프리뷰 기능

---
+++ Assets/Scripts/SetObjectPreview.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObjectPreview : MonoBehaviour {
+	public GameObject target;
+
+	void Start ()
+	{
+		target = Instantiate(target);
+		target.transform.parent = transform;
+		target.transform.localPosition = new Vector3(-230,0,0);
+
+	}
+	void Update ()
+	{
+		target.transform.Rotate(Vector3.up);
+	}
+}
